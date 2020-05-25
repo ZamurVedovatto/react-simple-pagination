fe007cca1cab77145d79ba9e8e07c4aac506561c
Zamur Vedovatto <vedovatto@gmail.com>
2020-05-25 12:26:40 -0300

    set page limit to 10

--- src/App.js
+++ src/App.js
@@ -16,16 +16,18 @@ function App() {
       setPosts(res.data);
       setLoading(false);
     }
-
     fetchPosts();
   }, []);
 
-  console.log(posts)
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
     <div className="container mt-2">
       <h1 className="text-primary mb-1">Posts List</h1>
-      <Posts posts={posts} loading={loading} />
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 }
